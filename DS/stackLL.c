61e80d6ce2f8b265aedd4cf3e6962922372f5672
niluoo <nilursrch@gmail.com>
2024-11-11 12:20:59 +0530

    Update stackLL.c

--- DS/stackLL.c
+++ DS/stackLL.c
@@ -14,5 +14,43 @@ void create_node() {
 }
 
 int main() {
-  
+  head = (struct Node*) malloc(sizeof(struct Node));
+  head->link = NULL;
+
+  puts("");
+  puts("-- STACK OPERATIONS --");
+  puts("1. PUSH");
+  puts("2. POP");
+  puts("3. STATUS");
+  puts("4. DISPLAY");
+  puts("5. EXIT");
+  puts("");
+
+  int choice;
+  printf("Enter a choice");
+  do {
+   swith(choice) {
+     case 1:
+       push();
+       break;
+     case 2:
+       pop();
+       break;
+     case 3:
+       status();
+       break;
+     case 4:
+       display();
+       break;
+     case 5:
+       puts("");
+       puts("-- EXITED --");
+       puts("");
+       exit(0);
+     default:
+       puts("");
+       puts("Invalid choice!");
+       puts("");
+   }
+  } while(choice != 5);
 }
