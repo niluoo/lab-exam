3c9b08513b5b99d5b54eb1ede0a5901c47ae3a2f
niluoo <nilursrch@gmail.com>
2024-11-13 01:31:00 +0530

    Update quick_sort.c

--- DS/quick_sort.c
+++ DS/quick_sort.c
@@ -1,5 +1,32 @@
 #include <stdio.h>
 
+void swap(int *a, int *b) {
+  int tmp = *a;
+  *a = *b;
+  *b = tmp;
+}
+
+int partition(int a[], int low, int high) {
+  int pivot = a[high];
+  int i = (low - 1);
+  for(int j = low; j < high; ++j) {
+    if(a[j] <= pivot) {
+      i++;
+      swap(&a[i], &a[j]);
+    }
+  }
+  swap(&a[i+1], &a[high]);
+  return (i + 1);
+}
+
+void quick_sort(int a[], int low, int high) {
+  if(low < high) {
+    int pos = partition(a, low, high);
+    quick_sort(a, low, pos-1);
+    quick_sort(a, pos+1, high);
+  }
+}
+
 int main() {
   printf("Enter number of elements: ");
   int n;
@@ -9,7 +36,12 @@ int main() {
   for(int i = 0; i < n; ++i) {
     scanf("%d", &a[i]);
   }
-  
+  quick_sort(a, 0, n-1);
+  printf("Sorted array: ");
+  for(int i = 0; i < n; ++i) {
+    printf("%d ", a[i]);
+  }
+  puts("");
   
   return 0;
 }
