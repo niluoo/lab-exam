f313ee9bfe053470be7c911b595df599c324ae93
niluoo <nilursrch@gmail.com>
2024-11-12 11:34:22 +0530

    Update circular_queue.c

--- DS/circular_queue.c
+++ DS/circular_queue.c
@@ -6,7 +6,52 @@
 int cq[nax];
 int front = -1, rear = -1;
 
+void enQueue() {
+  printf("Enter value to insert: ");
+  int value;
+  scanf("%d", &value);
+  if(front == -1 && rear == -1) {
+    front = rear = 0;
+    cq[rear] = value;
+    printf("%d inserted\n", value);
+  }
+  else if((rear + 1) % nax == front) {
+    puts("Queue full, insertion not possible!");
+  }
+  else {
+    rear = (rear + 1) % nax;
+    cq[rear] = value;
+    printf("%d inserted\n", value);
+  }
+}
 
+void deQueue() {
+  if(front == -1 && rear == -1) {
+    puts("Queue empty, deletion not possible");
+  }
+  else if(front == rear) {
+    printf("%d deleted\n", cq[front]);
+    front = -1;
+    rear = -1;
+  }
+  else {
+    printf("%d deleted\n", cq[front]);
+    front = (front + 1) % nax;
+  }
+}
+
+void display() {
+  if(front == -1 && rear == -1) {
+    puts("QUEUE EMPTY");
+  }
+  else {
+    printf("Queue: ");
+    for(int i = front; i != rear; i = (i+1) % nax) {
+      printf("%d ", cq[i]);
+    }
+    printf("%d\n", cq[rear]);
+  }
+}
 
 int main() {
   puts("-- CIRCULAR QUEUE --");
